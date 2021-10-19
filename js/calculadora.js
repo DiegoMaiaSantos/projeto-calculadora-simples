f61d9ddb3e199128fc6968a3d7f05692ddbffdae
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-10-19 00:43:12 -0300

    Projeto calculadora finalizado.

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,5 +1,23 @@
-function insert(num1) 
-{
-let num2 = document.getElementById('resultado').innerHTML;
-document.getElementById('resultado').innerHTML = num2 + num1;
+function insert(num1) {
+    let num2 = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = num2 + num1;
+}
+
+function clean() {
+    document.getElementById('resultado').innerHTML = '';
+}
+
+function back() {
+    let resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+}
+
+function calcular() {
+    let resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else {
+        document.getElementById('resultado').innerHTML = '0';
+    }
 }
